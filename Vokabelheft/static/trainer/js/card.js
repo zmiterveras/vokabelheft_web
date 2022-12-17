26efd03df9e92bd8830449276e02b25c680119c4
ZMV <zmvph79@gmail.com>
2022-12-17 21:02:40 +0300

    corrected card.js and cards.html

--- Vokabelheft/static/trainer/js/card.js
+++ Vokabelheft/static/trainer/js/card.js
@@ -4,17 +4,17 @@ var my_word = word.text();
 var my_key = key.text();
 
 $('div#card').hide();
-$('p#key').text(my_key);
-$('p#word').text(my_word).hide();
+$('h#key').text(my_key);
+$('h#word').text(my_word).hide();
 var myswitch = 1;
 $('div.card-body').on('click', function() {
     if (myswitch == 1) {
-        $('p#key').fadeOut(1500);
-        $('p#word').delay(1490).fadeIn(1500);
+        $('h#key').fadeOut(1500);
+        $('h#word').delay(1490).fadeIn(1500);
         myswitch = 0;
     } else {
-        $('p#word').fadeOut(1500);
-        $('p#key').delay(1490).fadeIn(1500);
+        $('h#word').fadeOut(1500);
+        $('h#key').delay(1490).fadeIn(1500);
         myswitch = 1;
     }
 });
