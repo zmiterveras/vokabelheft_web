ebeec9d54f95e8d642a74a8cb85d0fc70fedecdd
ZMV <zmvph79@gmail.com>
2023-03-17 12:49:40 +0300

    added get environments variables from .env (django-environ) added gunicorn server

--- Vokabelheft/Vokabelheft/settings.py
+++ Vokabelheft/Vokabelheft/settings.py
@@ -9,18 +9,24 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_5@r0m@86&a3#7usu8k$sy=na%+#u8ijo2zzf8%g^f0pa5_csi'
+# SECRET_KEY = 'django-insecure-_5@r0m@86&a3#7usu8k$sy=na%+#u8ijo2zzf8%g^f0pa5_csi'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
